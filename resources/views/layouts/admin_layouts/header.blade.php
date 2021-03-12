dc127340b8622df74a6dce2cc08f7eae6404bd9d
Chris Miracle <55501479+Chris-Miracle@users.noreply.github.com>
2021-03-12 08:31:55 +0100

    Admin login

---
+++ resources/views/layouts/admin_layouts/header.blade.php
@@ -0,0 +1,36 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+<link rel="icon" type="image/x-icon" href="admin/assets/img/favicon.ico"/>
+<link href="{{asset('admin/assets/css/loader.css')}}" rel="stylesheet" type="text/css" />
+<script src="{{asset('admin/assets/js/loader.js')}}"></script>
+
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
+<link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+<link href="{{asset('admin/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('admin/assets/css/plugins.css')}}" rel="stylesheet" type="text/css" />
+<!-- END GLOBAL MANDATORY STYLES -->
+
+<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+<link href="{{asset('admin/plugins/apex/apexcharts.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('admin/assets/css/dashboard/dash_2.css')}}" rel="stylesheet" type="text/css" />
+<!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+
+<link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"/>
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
+<link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+<link href="{{asset('admin/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('admin/assets/css/plugins.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('admin/plugins/perfect-scrollbar/perfect-scrollbar.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('admin/assets/css/authentication/form-2.css')}}" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="{{asset('admin/assets/css/all.min.css')}}" type="text/css">
+<link rel="stylesheet" href="{{asset('admin/font/font-awesome.min.css')}}" type="text/css">
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/css/forms/theme-checkbox-radio.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/css/forms/switches.css')}}">
+
+<!-- END GLOBAL MANDATORY STYLES -->
+
+<!--  BEGIN CUSTOM STYLE FILE  -->
+<link href="{{asset('admin/assets/css/apps/invoice.css" rel="stylesheet" type="text/css')}}" />
+<!--  END CUSTOM STYLE FILE  -->
